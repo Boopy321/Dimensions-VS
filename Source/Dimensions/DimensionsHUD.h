f1d4f466c43d5a0ad50db59a4a9dc233b23f4e8b
Jason Butterfield <butters_jnr@hotmail.com>
2015-08-05 12:37:44 +1000

    Removing Traces of last project

---
+++ Source/Dimensions/DimensionsHUD.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "DimensionsHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIMENSIONS_API ADimensionsHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
